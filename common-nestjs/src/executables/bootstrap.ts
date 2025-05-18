c45b7375f36082ef65b926af3c0316e512ad0ca1
Marc Schroeder <marc.schroeder@kominal.com>
2025-05-18 09:40:53 +0200

    Extend bootstrap

--- common-nestjs/src/executables/bootstrap.ts
+++ common-nestjs/src/executables/bootstrap.ts
@@ -1,6 +1,7 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { Test, TestingModule } from '@nestjs/testing';
 import { writeFileSync } from 'fs';
 import { exit } from 'process';
 import { CommonExceptionFilter } from '../helpers/exception-filter';
@@ -10,29 +11,42 @@ process.on('uncaughtException', (exception) => {
   console.log(exception);
 });
 
-export async function bootstrap(module: any, bodyParser = true): Promise<void> {
-  const app = await NestFactory.create(module, {
-    logger: new CommonLogger(),
-    cors: { origin: '*' },
-    bodyParser,
-  });
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new CommonExceptionFilter());
-  app.enableCors();
-
+function getOpenAPIObject(app: INestApplication): OpenAPIObject {
   const config = new DocumentBuilder()
     .setTitle('Core')
     .addServer('/api/core', 'Current installation')
     .addBearerAuth() //
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app as any, document);
+  return SwaggerModule.createDocument(app, config);
+}
 
+export function findControllers(module: any): any[] {
+  const reflector = new Reflector();
+  const controllers = reflector.get<any[]>('controllers', module) || [];
+  const imports = reflector.get<any[]>('imports', module) || [];
+  return [...imports.reduce((acc: any[], mod: any) => [...acc, ...findControllers(mod)], []), ...controllers];
+}
+
+export async function bootstrap(module: any, bodyParser = true): Promise<void> {
   if (process.env.GENERATE_SWAGGER) {
-    writeFileSync('swagger.json', JSON.stringify(document));
+    const moduleRef: TestingModule = await Test.createTestingModule({ controllers: findControllers(module) })
+      .useMocker(() => ({}))
+      .compile();
+    const app = moduleRef.createNestApplication();
+
+    writeFileSync('swagger.json', JSON.stringify(getOpenAPIObject(app), null, 2));
     exit(0);
   } else {
+    const app = await NestFactory.create(module, {
+      logger: new CommonLogger(),
+      cors: { origin: '*' },
+      bodyParser,
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new CommonExceptionFilter());
+    app.enableCors();
+    SwaggerModule.setup('api', app, getOpenAPIObject(app));
     await app.listen(3000);
   }
 }
